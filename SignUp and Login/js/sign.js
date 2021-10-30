3a86bd2db6718103f6fe18ff82151f9880d51fd3
SkullCreek <darpan.bahadur.2001@gmail.com>
2021-10-30 16:01:20 +0530

    login page open + responsive + slider done

---
+++ SignUp and Login/js/sign.js
@@ -0,0 +1,61 @@
+//signup login animation
+
+var signup_btn = document.getElementById("sign-up-btn");
+signup_btn.onclick = function(){
+    var slider = document.getElementById("bg-img");
+    slider.style.animation = "slide 2s";
+    slider.style.animationFillMode = "forwards";
+    return false;
+}
+var login_btn = document.getElementById("login-btn");
+login_btn.onclick = function(){
+    var slider = document.getElementById("bg-img");
+    slider.style.animation = "slide2 2s";
+    slider.style.animationFillMode = "forwards";
+    return false;
+}
+
+
+function myFunction(x) {
+    if (x.matches) {
+        var signup_btn = document.getElementById("sign-up-btn");
+        signup_btn.onclick = function(){
+            var slider = document.getElementById("bg-img");
+            slider.style.animation = "mobile 2s";
+            slider.style.animationFillMode = "forwards";
+            return false;
+        }
+        var login_btn = document.getElementById("login-btn");
+        login_btn.onclick = function(){
+        var slider = document.getElementById("bg-img");
+        slider.style.animation = "mobile2 2s";
+        slider.style.animationFillMode = "forwards";
+        return false;
+}
+    } 
+  }
+  
+  var x = window.matchMedia("(max-width: 650px)");
+  myFunction(x);
+
+function login_check(){
+    var login = sessionStorage.getItem("sign");
+    window.alert(login);
+    if(login== "Login"){
+        var slider = document.getElementById("bg-img");
+        slider.style.right="50%";
+        function myFunction(x){
+            if (x.matches) {
+                var slider = document.getElementById("bg-img");
+                slider.style.transform = "translateY(0px)";
+                
+            }
+        }
+        var x = window.matchMedia("(max-width: 650px)");
+        myFunction(x);
+        sessionStorage.removeItem("sign");
+    }
+
+}
+
+login_check();

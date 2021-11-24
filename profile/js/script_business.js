69bb56ffb8537c0d6fc00068d7d86b1a8c76c8a2
Darpanarayan Bahadur <darpan.bahadur.2001@gmail.com>
2021-11-24 17:03:30 +0530

    initial commit of business page

---
+++ profile/js/script_business.js
@@ -0,0 +1,21 @@
+//profile photo 
+
+function showing_pic_name(){
+    var name  = document.getElementById("user-name");
+    var user_mail = sessionStorage.getItem("user_mail");
+    var emailid = document.getElementById("email-id");
+    var user_details = localStorage.getItem(user_mail);
+    var user_data= JSON.parse(user_details);
+    var fullname = user_data.name;
+    name.innerHTML=atob(fullname);
+    emailid.innerHTML=atob(user_mail);
+
+    var pic_box = document.getElementById("profile-img-section");
+    var image_name = localStorage.getItem(user_mail+"image_url");
+    pic_box.style.background="url("+image_name+")";
+    pic_box.style.backgroundRepeat="no-repeat";
+    pic_box.style.backgroundSize="contain";
+}
+
+showing_pic_name();
+//profile photo
